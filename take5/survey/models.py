b447b704a6d5f4d16a29bf0bd4b9f9c8e259a5fa
itgabriel <gabriel.gustavo.s@hotmail.com>
2021-04-25 04:02:20 -0300

    Models str
    Tornar possível a leitura do objeto ao usuário final

--- take5/survey/models.py
+++ take5/survey/models.py
@@ -7,17 +7,26 @@ class Survey(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=20)
     description = models.TextField(max_length=120)
+
+    def __str__(self):
+        return f"{self.name}"
     
 class SurveyQuestion(models.Model):
     id = models.BigAutoField(primary_key=True)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     question = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.question}"
     
 class SurveyQuestionAlternative(models.Model):
     id = models.BigAutoField(primary_key=True)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     survey_question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     question_alternative = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.question_alternative}"
    
 class SurveyUserAnswer(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -26,3 +35,6 @@ class SurveyUserAnswer(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     survey_question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     survey_question_alternative = models.ForeignKey(SurveyQuestionAlternative, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.survey_question_alternative}"
