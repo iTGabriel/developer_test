139dbe3881a39b1d53d582addb469c921857a5aa
itgabriel <gabriel.gustavo.s@hotmail.com>
2021-04-27 01:55:08 -0300

    Templates demo_views
    Templates para uma primeira visualização dos dados consumidos da api

--- take5/survey/services/request_api.py
+++ take5/survey/services/request_api.py
@@ -6,3 +6,24 @@ def get_surveys():
     except:
         dados = []
     return dados
+
+def get_surveys_questions():
+    try:
+        dados = requests.get('http://localhost:8000/api/surveys/questions//').json()
+    except:
+        dados = []
+    return dados
+
+def get_surveys_questions_alternatives():
+    try:
+        dados = requests.get('http://localhost:8000/api/surveys/questions/alternatives/').json()
+    except:
+        dados = []
+    return dados
+
+def get_surveys_user_answer():
+    try:
+        dados = requests.get('http://localhost:8000/api/surveys/user/answer/').json()
+    except:
+        dados = []
+    return dados
--- take5/survey/views.py
+++ take5/survey/views.py
@@ -3,6 +3,10 @@ from .services import request_api
 # Create your views here.
 
 def home(request):
-    dados = request_api.get_surveys()
-    return render(request, 'index.html', {'dados': dados} )
+    dados_surveys = request_api.get_surveys()
+    dados_questions = request_api.get_surveys_questions()
+    dados_alternatives = request_api.get_surveys_questions_alternatives()
+    dados_user_answer = request_api.get_surveys_user_answer()
+
+    return render(request, 'index.html', {'dados_surveys': dados_surveys, 'dados_questions': dados_questions, 'dados_alternatives': dados_alternatives, 'dados_user_answer': dados_user_answer} )
 
